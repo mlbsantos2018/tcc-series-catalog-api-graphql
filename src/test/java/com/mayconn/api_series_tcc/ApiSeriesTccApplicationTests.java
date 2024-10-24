2768c05c4f486bedc2131eb86e5535eb615a636a
mlbsantos2018 <mlbsantos2018>
2024-10-23 22:44:07 -0300

    Adiciona o diretório padrão de testes

---
+++ src/test/java/com/mayconn/api_series_tcc/ApiSeriesTccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayconn.api_series_tcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSeriesTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
