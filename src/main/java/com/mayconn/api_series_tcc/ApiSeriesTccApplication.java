d98025e109e5ac7a7f832592c4e7af4c40ddda58
mlbsantos2018 <mlbsantos2018>
2024-10-23 22:49:08 -0300

    Adiciona classe main do projeto

---
+++ src/main/java/com/mayconn/api_series_tcc/ApiSeriesTccApplication.java
@@ -0,0 +1,13 @@
+package com.mayconn.api_series_tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSeriesTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSeriesTccApplication.class, args);
+	}
+
+}
